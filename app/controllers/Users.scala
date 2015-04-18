a54e43fdb0bbd7f126f860e847672237956d059e
Chris Coffey <ccoffey@leaf.me>
2015-04-18 11:17:16 -0400

    clean up routes

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,9 +1,32 @@
-package controllers
+package proxy.controllers
 
 import play.api.mvc._
 
 object Users extends Controller {
 
+  def all = ????
+  def user(id: String) = ????
+  def channels(id: String) = ????
+  def channel(id: String, cid: String) = ????
+  def contacts(id: String) = ????
+  def groups(id: String) = ????
+  def group(id: String, gid: String) = ????
+  def groupContacts(id: String, gid: String) = ????
+  def groupChannels(id: String, gid: String) = ????
 
+  def newChannel(id: String) = ????
+  def newGroup(id: String) = ????
+  def newGroupContact(id: String, gid: String) = ????
+  def newGroupChannel(id: String, gid: String) = ????
 
+  def removeChannel(id: String, cid: String) = ????
+  def removeGroup(id: String, gid: String) = ????
+  def removeGroupContact(id: String, gid: String, cid: String) = ????
+  def removeGroupChannel(id: String, gid: String, cid: String) = ????
+
+  def replaceContext(id: String) = ????
+
+  private def ???? = Action {
+    Forbidden
+  }
 }
